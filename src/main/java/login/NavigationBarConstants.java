f2904363cd04e02cbcca3e1ef116a8a3100d489b
hellosold <hellolily0511@gmail.com>
2021-12-06 20:48:34 -0800

    added navigation bar

---
+++ src/main/java/login/NavigationBarConstants.java
@@ -0,0 +1,35 @@
+package login;
+
+public class NavigationBarConstants {
+    public static String NAVI_STYLE = "<style>\n" +
+            "ul {\n" +
+            "  list-style-type: none;\n" +
+            "  margin: 0;\n" +
+            "  padding: 0;\n" +
+            "  overflow: hidden;\n" +
+            "  background-color: #bbded6;\n" +
+            "}\n" +
+            "\n" +
+            "li {\n" +
+            "  float: left;\n" +
+            "}\n" +
+            "\n" +
+            "li a {\n" +
+            "  display: block;\n" +
+            "  color: white;\n" +
+            "  text-align: center;\n" +
+            "  padding: 14px 16px;\n" +
+            "  text-decoration: none;\n" +
+            "}\n" +
+            "\n" +
+            "li a:hover:not(.active) {\n" +
+            "  background-color: #bbded6;\n" +
+            "}\n" +
+            "\n" +
+            ".active {\n" +
+            "  background-color: #8ac6d1;\n" +
+            "}\n" +
+            "</style>";
+
+
+}
