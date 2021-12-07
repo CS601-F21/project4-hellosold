09d007a76934d3394ed0e71f7e3a0d5e7a2af683
hellosold <hellolily0511@gmail.com>
2021-12-06 23:25:17 -0800

    test of AddEventServlet

---
+++ src/test/java/AddEventServletTest.java
@@ -0,0 +1,84 @@
+import org.eclipse.jetty.http.HttpStatus;
+import org.junit.Test;
+import utilities.Utilities;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddEventServletTest {
+    private String url = "https://61ec-67-169-155-8.ngrok.io/events/add";
+
+    @Test
+    // Test valid xhtml file of get request when user not logged in
+    public void testGetAddAEvent() {
+        try {
+            HttpRequest.Builder builder = HttpRequest.newBuilder(new URI(url));
+            HttpRequest request = builder.GET()
+                    .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String resBody = response.body();
+
+            boolean actual = Utilities.isValid(resBody);
+            assertTrue(actual);
+        } catch (IOException | URISyntaxException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    // Test status code of a valid get request when user not logged in
+    public void testGetStatusCode() {
+        try {
+            HttpRequest.Builder builder = HttpRequest.newBuilder(new URI(url));
+            HttpRequest request = builder.GET()
+                    .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = response.statusCode();
+            assertEquals(HttpStatus.OK_200, statusCode);
+        } catch (IOException | URISyntaxException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    // Test valid xhtml page of a get request
+    public void testGetValidXHtml() {
+        try {
+            HttpRequest.Builder builder = HttpRequest.newBuilder(new URI(url)).setHeader("cookie", Utilities.session);
+            HttpRequest request = builder.GET()
+                    .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String resBody = response.body();
+            assertTrue(Utilities.isValid(resBody));
+        } catch (IOException | URISyntaxException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    // Test valid xhtml page of a get request
+    public void testGetStatusCodeLoggedIn() {
+        try {
+            HttpRequest.Builder builder = HttpRequest.newBuilder(new URI(url)).setHeader("cookie", Utilities.session);
+            HttpRequest request = builder.GET()
+                    .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            int statusCode = response.statusCode();
+            assertEquals(HttpStatus.OK_200, statusCode);
+        } catch (IOException | URISyntaxException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
