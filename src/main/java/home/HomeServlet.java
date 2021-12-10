c60a9add26e20e6051a1efae2c01ac027a5534b2
hellosold <hellolily0511@gmail.com>
2021-12-09 16:53:34 -0800

    added transfer fragment

--- src/main/java/home/HomeServlet.java
+++ src/main/java/home/HomeServlet.java
@@ -11,11 +11,14 @@ import login.LoginServerConstants;
 import login.NavigationBarConstants;
 import org.eclipse.jetty.http.HttpStatus;
 import tickets.Ticket;
+import tickets.TransferTicket;
+import utilities.Utilities;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +30,7 @@ import java.util.Map;
  */
 public class HomeServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         // determine whether the user is already authenticated
         Object clientInfoObj = req.getSession().getAttribute(LoginServerConstants.CLIENT_INFO_KEY);
         PrintWriter writer = resp.getWriter();
@@ -38,20 +41,17 @@ public class HomeServlet extends HttpServlet {
             String id = String.valueOf(data.get("id"));
             int userId = Integer.parseInt(id);
             String name = data.get("name");
-
             resp.setStatus(HttpStatus.OK_200);
             writer.println(HomeServletConstants.PAGE_HEADER);
             writer.println(NavigationBarConstants.NAVI_STYLE);
             writer.println(HomeServletConstants.NAVI_BODY);
             writer.println("<h1>Hello, " + name + "</h1>");
             getAndPrintPurchasedTickets(userId, writer);
+            getAndPrintALlTransferTickets(userId, writer);
             writer.println(HomeServletConstants.PAGE_FOOTER);
             return;
         }
-        resp.setStatus(HttpStatus.OK_200);
-        writer.println(LoginServerConstants.PAGE_HEADER);
-        writer.println("<h1>Please log in</h1>");
-        writer.println(LoginServerConstants.PAGE_FOOTER);
+        Utilities.printRequireLogInPage(resp);
     }
 
     /**
@@ -60,22 +60,54 @@ public class HomeServlet extends HttpServlet {
      * @param writer wroter
      */
     private void getAndPrintPurchasedTickets(int userId, PrintWriter writer) {
+        List<Ticket> tickets = assembleTickets(userId);
+        Event event;
+        if (tickets.size() != 0) {
+            writer.println("<h2>Tickets:<h2>");
+            for (Ticket t : tickets) {
+                event = t.getEvent();
+                writer.println("  <h4>Title:" + event.getTitle() + "</h4>");
+                writer.println("    <p> Date: " + event.getDate() + "</p>");
+                writer.println("    <p> Time: " + event.getTime() + "</p>");
+                writer.println("    <p> Place: " + event.getPlace() + "</p>");
+                writer.println("    <p> Tickets left: " + event.getTickets() + "</p>");
+                writer.println("    <p> Owned tickets: " + t.getTickets() + "</p>");
+            }
+            writer.println("<br />");
+        }
+    }
+
+    /**
+     * Sum of tickets for all events that the user has purchased.
+     * @param userId user id
+     * @return list of tickets
+     */
+    private List<Ticket> assembleTickets(int userId) {
+        List<Ticket> tickets = new ArrayList<>();
+        // get tickets from database
         try (Connection con = DBCPDataSource.getConnection()) {
-            List<Ticket> tickets = JDBCUtility.executeGetAllPurchasedTickets(con, userId);
-            if (tickets.size() != 0) {
-                writer.println("<h2>Tickets:<h2>");
-                Event event;
-                for (Ticket t : tickets) {
-                    event = t.getEvent();
-                    writer.println("  <h4>Title:" + event.getTitle() + "</h4>\n");
-                    writer.println("    <p> Date: " + event.getDate() + "</p>\n");
-                    writer.println("    <p> Time: " + event.getTime() + "</p>\n");
-                    writer.println("    <p> Place: " + event.getPlace() + "</p>\n");
-                    writer.println("    <p> Tickets left: " + event.getTickets() + "</p>\n");
-                    writer.println("    <p> Description: " + event.getDescription() + "</p>\n");
-                    writer.println("    <p> Owned tickets: " + t.getTickets() + "</p>\n");
+            tickets = JDBCUtility.executeGetAllPurchasedTickets(con, userId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tickets;
+    }
+
+    /**
+     * A helper to get and print all transfer tickets.
+     * @param userId userId
+     * @param writer writer
+     */
+    private void getAndPrintALlTransferTickets(int userId, PrintWriter writer) {
+        try (Connection con = DBCPDataSource.getConnection()) {
+            List<TransferTicket> transTik = JDBCUtility.executeGetAllTransfers(con, userId);
+            if (transTik.size() != 0) {
+                for (TransferTicket ticket : transTik) {
+                    writer.println("<h2>Transfer tickets: </h2>");
+                    writer.println("<h4>Title: " + ticket.getEventTitle() + "</h4>");
+                    writer.println("  <p> Transfer to user: " + ticket.getToUser() + "</p>");
+                    writer.println("  <p> Number of Tickets: " + ticket.getNum() + "</p>");
                 }
-                writer.println("<br />");
             }
         } catch (SQLException e) {
             e.printStackTrace();
